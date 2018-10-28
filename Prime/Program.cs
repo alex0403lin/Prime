508cdfbf5df4deb362159a6e5a23754885016b75
xupmpl123 <xupmpl123@gmail.com>
2018-10-29 00:19:26 +0800

    加入專案檔案。

---
+++ Prime/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime;
+            int number = 100;
+            int i, j;
+
+            for (i = 2; i <= number; i++)
+            {
+                isPrime = true;
+                for (j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime == true)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
